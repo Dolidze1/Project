2a8d58eb7ffa9bcf8f79b093bb0abb3c2b31bc3d
Giga <gigadolidze@gmail.com>
2021-03-22 23:35:20 +0400

    fsad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ToursComponent } from './tours/tours.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'' ,redirectTo: '/home' , pathMatch:'full'},
+  {path:'home' ,component: HomeComponent},
+  {path:'about' ,component: AboutComponent},
+  {path:'contact' ,component: ContactComponent},
+  {path:'tours' ,component: ToursComponent},
+  {path:'login' ,component: LoginComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ToursComponent } from './tours/tours.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    ToursComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
