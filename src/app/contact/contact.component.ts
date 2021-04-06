beabec58a4669e06bd701bfd01301f0e3323c5b0
Giga <gigadolidze@gmail.com>
2021-04-06 20:30:41 +0400

    123

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -10,8 +10,15 @@ export class ContactComponent implements OnInit {
 
   
   showError:boolean = false;
-  errorText: string = '';
+  
   showError2 : boolean = false;
+  contactName : string = ''
+  contactLastName : string = ''
+  contactEmail : string = ''
+  contactCountry : string = ''
+  
+
+
  
   variety: string[] = ["Georgia, ", 'Spain', 'Italy', 'United Kingdom' , 'Greec','Japan'];
 
@@ -30,8 +37,68 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  sendContact(){
+  
+  sendContact()
+  {
+    
+    
+    var formValues = this.contactForm.value
+    var name  = formValues.nameControl;
+    var lastname = formValues.lastNameControl;
+    var email = formValues.emailControl;
+    var variety = formValues.varietyControl;
+    
+    
+    if (this.contactName == "") {
+
+
+      this.showError = true;
+
+
+    }
+    else if (this.contactLastName == "") {
+      this.showError = true;
+      
+
+    }
+    else if (this.contactEmail == "") {
+      this.showError = true;
+      
+
+    }
+    
+    
+
+    
+
+    else if (this.contactCountry == '') {
+      this.showError = true;
+     
+
+    }
+    else if (this.contactName.trim().toLowerCase() == this.contactLastName.trim().toLowerCase()) {
+      this.showError = true;
+      
+
+
+    }
+
+    else if (this.contactEmail.indexOf("@") == -1 || this.contactEmail.indexOf('.') == -1) {
+      this.showError = true;
+      
+
+    }
+
+    else {
+      this.showError = false;
+     
+      
+    }
 
   }
+  
 
+   
+     
+     
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+ 
+  contactEmail:string= '';
+  contactPassword:string=''
+  showError:boolean= false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+   sendContact(){
+
+  
+    
+     if (this.contactEmail == "") {
+      this.showError = true;
+      
+
+    }
+    
+   else  if (this.contactPassword == "") {
+      this.showError = true;
+      
+
+    }
+
+
+    else if (this.contactEmail.indexOf("@") == -1 || this.contactEmail.indexOf('.') == -1) {
+      this.showError = true;
+      
+
+    }
+
+    else {
+      this.showError = false;
+     
+      
+    }
+
+  }
+  
+   }
+
